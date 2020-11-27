5d4604231161af561178c4d1a44010f129a486c9
jayaston <23299434@qq.com>
2020-11-27 17:05:37 +0800

    up

--- 查询台账任意数据.py
+++ 查询台账任意数据.py
@@ -24,10 +24,10 @@ import datetime as dt
 
 
 list1 = [         
-         ['1009','04281','m']
+         ['00','00718','d']
         
          ]
-shuju_df = tjfx.TjfxData().getdata('20200101','20200930',list1)
+shuju_df = tjfx.TjfxData().getdata('20190101','20191231',list1)
 
 shuju_df.info()
 
@@ -43,22 +43,27 @@ test = test.resample('Y').sum()
 test.to_excel(r'C:\Users\XieJie\mypyworks\输出\2019年各厂各月取供比.xlsx')
 
 #求最大日
-list2 = [['1009','04281','d']
-         
+list2 = [['1001','00718','d'],
+         ['1002','00718','d'],
+         ['1003','00718','d'],
+         ['1016','00718','d'],
+         ['1004','00718','d'],
+         ['1005','00718','d'],
+         ['1007','00718','d']         
          ]
-shuju_df = tjfx.TjfxData().getdata('20140101','20191231',list2)
+shuju_df = tjfx.TjfxData().getdata('20190101','20191231',list2)
 shuju_df.info()
 shuju_df.QUOTA_VALUE = pd.to_numeric(shuju_df.QUOTA_VALUE,errors='coerce').fillna(0)
 
-shuju_df2 = pd.pivot_table(shuju_df,index=['QUOTA_DATE'],columns=[ 'QUOTA_NAME'],values= 'QUOTA_VALUE',
-                          aggfunc=np.sum)
-shuju_df2.reset_index(inplace=True)
-shuju_df2.info()
+# shuju_df2 = pd.pivot_table(shuju_df,index=['QUOTA_DATE'],columns=[ 'QUOTA_NAME'],values= 'QUOTA_VALUE',
+#                           aggfunc=np.sum)
+# shuju_df2.reset_index(inplace=True)
+# shuju_df2.info()
 #按年最大值
-get_max = lambda x: x[x['入厂取水量']==x['入厂取水量'].max()][['QUOTA_DATE','入厂取水量']]
+get_max = lambda x: x[x['QUOTA_VALUE']==x['QUOTA_VALUE'].max()][['QUOTA_DATE','QUOTA_VALUE']]
                         
 # python就是灵活啊。
 get_max.__name__ = "maxday"
-test = shuju_df2.groupby([pd.Grouper(key='QUOTA_DATE',freq='Y')]).apply(get_max)
+test = shuju_df.groupby([pd.Grouper(key='QUOTA_DATE',freq='Y'),'GROUP_NAME']).apply(get_max)
 
-
+test.to_excel(r'C:\Users\XieJie\mypyworks\输出\2019年各厂最高日供水总量.xlsx')
